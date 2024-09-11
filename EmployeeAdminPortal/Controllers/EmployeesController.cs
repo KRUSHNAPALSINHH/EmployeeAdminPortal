0b40a2aba0db461dbe61c8e1ecd0e4a9e1055a1c
Dell <krushnapalsinhvaghela4@gmail.com>
2024-09-11 10:53:44 +0530

    Add project files.

---
+++ EmployeeAdminPortal/Controllers/EmployeesController.cs
@@ -0,0 +1,83 @@
+﻿using EmployeeAdminPortal.Data;
+using EmployeeAdminPortal.Models;
+using EmployeeAdminPortal.Models.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeAdminPortal.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public EmployeesController(ApplicationDbContext dbContext) {
+            this.dbContext = dbContext;
+        }
+        [HttpGet]
+        public IActionResult GetAllEmployees()
+        {
+           var allEmployees= dbContext.Employees.ToList(); 
+            return Ok(allEmployees);
+        }
+        [HttpGet]
+        [Route("{id:int}")]
+        public IActionResult GetAllEmployeeById(int id)
+        {
+            var employees = dbContext.Employees.Find(id);
+            if (employees == null)
+            {
+                return NotFound();
+            }
+            return Ok(employees);
+        }
+
+        [HttpPost]
+        public IActionResult AddEmployee(AddEmployeeDto addEmployeeDto)
+        {
+            var employeeEntity = new Employee()
+            {
+                Name = addEmployeeDto.Name,
+                Email = addEmployeeDto.Email,
+                Phone = addEmployeeDto.Phone,
+                Salary = addEmployeeDto.Salary
+            };
+            dbContext.Employees.Add(employeeEntity);
+            dbContext.SaveChanges();
+            return Ok(employeeEntity);
+        }
+        [HttpPut]
+        [Route("{id:int}")]
+        public IActionResult UpdateEmployee(int id,UpdateEmployeeDto updateEmployeeDto)
+        {
+            var employee=dbContext.Employees.Find(id);
+            if (employee == null)
+            {
+                return NotFound(id);
+
+            }
+            employee.Name = updateEmployeeDto.Name;
+            employee.Email = updateEmployeeDto.Email;
+            employee.Phone = updateEmployeeDto.Phone;
+            employee.Salary = updateEmployeeDto.Salary;
+
+            dbContext.SaveChanges();
+            return Ok(employee);
+
+        }
+        [HttpDelete]
+        [Route("{id:int}")]
+        public IActionResult DeleteEmployee(int id)
+        {
+            var employee= dbContext.Employees.Find(id);
+            if (employee == null) 
+            { 
+               return NotFound();
+            }
+            dbContext.Employees.Remove(employee);
+            dbContext.SaveChanges();
+            return Ok();    
+        }
+    }
+}
